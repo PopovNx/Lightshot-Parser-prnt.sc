3aa9df329cdc7e997c3c816838f038ec0bc945e4
Андрей Попов <andreypopov2628007@gmail.com>
2020-10-31 12:26:13 +0200

    Add Prompt

--- prnt.sc Pars/Program.cs
+++ prnt.sc Pars/Program.cs
@@ -21,7 +21,7 @@ namespace prnt.sc_Pars
                 {
                     wc.Headers.Set(HttpRequestHeader.AcceptLanguage, "eu");
                     wc.Headers.Set(HttpRequestHeader.UserAgent, "Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10");
-                    Console.WriteLine($"GetPage {Url}");
+                    Console.Write ($"GetPage {Url} ");
                     var str = wc.DownloadString(Url);
                     var r = new Regex(@"<meta name=\""twitter:image:src\"" content=\""(.*?)\""\/>");
                     var m = r.Match(str).Groups[1].Value;
@@ -40,11 +40,14 @@ namespace prnt.sc_Pars
         static void Main(string[] args)
         {
             var Chars = "0123456789abcdefghijkmnopqrstuvwxyz";
-            var Start = "v83";
+            Console.Write("Enter Start letters (v83):");
+            var Start = Console.ReadLine();
             for (int ia = 0; ia < Chars.Length; ia++)
                 for (int ib = 0; ib < Chars.Length; ib++)
                     for (int ic = 0; ic < Chars.Length; ic++)
                         GetScrin($"https://prnt.sc/{Start}{Chars[ia]}{Chars[ib]}{Chars[ic]}");
+            Console.Beep();
+            Console.ReadLine();
         }
     }
 }
